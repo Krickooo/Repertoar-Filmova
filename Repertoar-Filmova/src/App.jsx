9812f9ad593677b60fe020a01ba40669319e45c9
Miloš Kričković <miloshino3@gmail.com>
2026-01-08 11:03:08 +0100

    App odrađen da prikazuje niz filmova za trenutni datum kreiranjem nove komponente i koriscenjem props-a

--- Repertoar-Filmova/src/App.jsx
+++ Repertoar-Filmova/src/App.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import Movies from "./Movies";
+import "./styles/main.scss";
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  const movies = [
+    {
+      title: "Captain America - The First Avenger",
+      hall: 2,
+      price: 350,
+    },
+    {
+      title: "The Papillon",
+      hall: 1,
+      price: 300,
+    },
+    {
+      title: "The Lost City of Z",
+      hall: 5,
+      price: 350,
+    },
+  ];
+
+ return (
+    <div>
+      <h1>Repertoar za danas ({new Date().toLocaleDateString("sr-RS")})</h1>
+      { movies.map(m => ( 
+        <Movies title={m.title} hall={m.hall} price={m.price} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
+
+
+
---
+++ Repertoar-Filmova/src/Movies.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Movies = (props) => {
+    return (
+        <div className="container">
+            <div className="image">
+                <img src="https://i.pinimg.com/736x/aa/f7/05/aaf705e06726ce3881288ae4be3ac5fe.jpg"/>
+            </div>
+            <div className="movie">
+                {props.title}, sala: {props.hall}, cena: {props.price}din
+            </div>
+        </div>
+    )
+}
+export default Movies;
